9a33926c87f0ae0d4f01d4395df061960349eee0
Jim Thorpe <jim.thorpe@canterbury.ac.nz>
2014-07-29 14:40:01 +1200

    some sort of YUI vaugely working

--- js/audiorecorder.js
+++ js/audiorecorder.js
@@ -1,4 +1,107 @@
 
+/* global YUI */
+YUI.add("audio-recorder", function (Y) {
+  "use strict";  
+ 
+  Y.AudioRecorder = Y.Base.create("audio-recorder", Y.Widget, [], {
+    initializer: function () {
+      // publish any events
+      // do any instantiation that doesn't require DOM
+    },
+    renderUI: function () {
+		// create all DOM nodes
+		var recordButton = Y.Node.create('<a class="btn btn-default" href="#" id="recordButton"><i class="fa fa-circle"></i></a>'),
+			playButton = Y.Node.create('<a class="btn btn-default playerControl disabled" href="#" id="playButton"><i class="fa fa-play"></i></a>'),
+			pauseButton = Y.Node.create('<a class="btn btn-default playerControl disabled" href="#" id="pauseButton"><i class="fa fa-pause"></i></a>'),
+			stopButton = Y.Node.create('<a class="btn btn-default disabled" href="#" id="stopButton"><i class="fa fa-stop"></i></a>'),
+			uploadButton = Y.Node.create('<a class="btn btn-default playerControl disabled" href="#" id="uploadButton"><i class="fa fa-upload"></i></a>');
+		      
+		// use this.getClassName(arg1s...) to create unque classes
+		// title.addClass(this.getClassName("title"));
+		// button.addClass(this.getClassName("button"));
+
+		// add nodes to the page all at once
+		this.get("container").append(Y.all([recordButton, playButton, pauseButton, stopButton, uploadButton]));
+		// console.log(Y.one('.videoRecorderControls'));
+		// Y.one('.videoRecorderControls').append(button);
+
+		// store shortcuts for DOM you'll need to reference
+		this._recordButton = recordButton;
+		this._playButton = playButton;
+		this._pauseButton = pauseButton;
+		this._stopButton = stopButton;
+		this._uploadButton = uploadButton;
+    },
+    bindUI: function () {
+      // store references to event handlers you set on other
+      // objects for clean up later
+      this._buttonClickHandler = this._buttonNode.on("click", function (event) {
+        Y.log("you clicked the button!");
+        event.halt();
+      }, this);
+      // assign listeners to events on the instance directly
+      // they're cleaned up automatically
+      this.after("titleChange", this._afterTitleChange, this);
+    },
+    _afterTitleChange: function (event) {
+      this._titleNode.setContent(event.newVal);
+    },
+    syncUI: function () {
+      // now that the DOM is created, syncUI sets it up
+      // given the current state of our ATTRS
+      this._afterTitleChange({
+        newVal: this.get("title")
+      });
+    },
+    destructor: function () {
+      if (this.get("rendered")) {
+        // bindUI was called, clean up events
+        this._buttonClickHandler.detach();
+        this._buttonClickHandler = null;
+        // renderUI was called, clean up shortcuts
+        this._titleNode = this._buttonNode = null;
+      }
+    }
+  }, {
+    // Public attributes that broadcast change events
+    ATTRS: {
+		title: {
+			value: "No one gave me a title :("
+		},
+		container : {
+	        value: null,
+	        setter: function(val) {
+	            return Y.one(val) || Y.one(".videoRecorderControls");
+	        }
+	    }
+    },
+    // Attributes whose default values can be scraped from HTML
+    HTML_PARSER: {
+      title: function (srcNode) {
+        return srcNode.getAttribute("title");
+      }
+    }
+  });
+
+}, "3.3.0", {
+  requires: [
+    "base-build", // provides Y.Base.create
+    "widget"      // provides Y.Widget
+  ],
+  group: "nfl",     // declares the nfl group (important for skins)
+  skinnable: false   // declares that your module is skinned
+});
+
+
+
+YUI().use("audio-recorder", function (Y) {
+    var AudioRecorder = new Y.AudioRecorder();
+    AudioRecorder.render();
+});
+
+
+
+
 
 var PlaybackControls = function(input) {
 
@@ -7,7 +110,7 @@ var PlaybackControls = function(input) {
     playback = new Playback();
     __log('Playback initialised.');
     record = new Record(input);
-    __log('Recorder initialised.');
+    __log('Record initialised.');
 
 	this.enablePlayerControls = function () {
 		Y.all('.playerControl').removeClass('disabled');
@@ -116,18 +219,3 @@ window.addEventListener ("message", function(e){
   }
 }, false);
 */
-
-
-Y = YUI().use('node', function (Y) {
-    Y.one('#recordButton').on('click', function(e) {
-	    e.preventDefault();
-	    console.log(recorder);
-	    if ( ! this.hasClass('recording') ) {
-	    // if ( ! recorder.recording ) {
-	    	// console.log(recorder.recording);
-	    	record.startRecording();
-	    } else {
-	    	record.stopRecording();
-	    }
-	});
-});
